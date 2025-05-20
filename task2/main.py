e1a17b244e5b149e17ae623c63febd5d551705b3
Vitalii Vasylets <mohara88s@gmail.com>
2025-05-20 16:53:27 +0300

    make task2 & fix task1 start task3

--- task2/main.py
+++ task2/main.py
@@ -1,4 +1,5 @@
 import networkx as nx
+from collections import deque
 
 # Створити граф
 G = nx.Graph()
@@ -6,7 +7,7 @@ G = nx.Graph()
 # Вершини (міста)
 cities = [
     "Київ", "Рівне", "Луцьк", "Житомир", "Львів", "Харків", "Одеса", "Дніпро", "Запоріжжя",
-    "Івано-Франківськ", "Тернопіль", "Кривий Ріг",
+    "Івано-Франківськ", "Тернопіль", "Кривий Ріг", "Ужгород", 
 ]
 
 # Додати вершини до графа
@@ -14,45 +15,58 @@ G.add_nodes_from(cities)
 
 # Ребра з вагами (приблизні відстані в км)
 edges = [
-    ("Київ", "Львів"),
     ("Київ", "Харків"),
-    ("Київ", "Одеса"),
-    ("Київ", "Дніпро"),
-    ("Київ", "Запоріжжя"),
     ("Київ", "Житомир"),
+    ("Київ", "Дніпро"),
     ("Рівне", "Луцьк"),
     ("Рівне", "Тернопіль"),
     ("Рівне", "Львів"),
-    ("Рівне", "Івано-Франківськ"),
-    ("Рівне", "Київ"),
     ("Рівне", "Житомир"),
-    ("Кривий Ріг", "Дніпро"),
     ("Кривий Ріг", "Запоріжжя"),
     ("Кривий Ріг", "Одеса"),
     ("Кривий Ріг", "Київ"),
     ("Житомир", "Рівне"),
-    ("Житомир", "Луцьк"),
     ("Житомир", "Тернопіль"),
     ("Луцьк", "Львів"),
     ("Львів", "Івано-Франківськ"),
     ("Львів", "Тернопіль"),
     ("Тернопіль", "Івано-Франківськ"),
-    ("Харків", "Дніпро"),
     ("Дніпро", "Запоріжжя"),
-    ("Одеса", "Запоріжжя")
+    ("Ужгород", "Львів"),
 ]
 
 # Додати ребра до графа
 G.add_edges_from(edges)
 
 
-def dfs_recursive(graph, vertex, visited=None):
+def dfs_recursive(graph, vertex, visited=None, path=None, parant=None):
     if visited is None:
         visited = set()
+        path = []
     visited.add(vertex)
-    print(vertex, end=' ')  # Відвідуємо вершину
+    # print(vertex, end=' ')  # Відвідуємо вершину
+    if parant is not None:
+        path.append((parant, vertex))
     for neighbor in graph[vertex]:
         if neighbor not in visited:
-            dfs_recursive(graph, neighbor, visited)
+            dfs_recursive(graph, neighbor, visited, path, vertex)
+    return path
 
-dfs_recursive(G, 'Рівне')
+def bfs_iterative(graph, start):
+    visited = set()
+    queue = deque([start])
+    path = []
+    while queue:
+        vertex = queue.popleft()
+        visited.add(vertex)
+        for neighbor in graph[vertex]:
+            if neighbor not in visited:
+                # print(neighbor, end=" ")
+                visited.add(neighbor)
+                queue.append(neighbor)
+                path.append((vertex, neighbor))
+    return path
+
+print(dfs_recursive(G, 'Рівне'))
+print()
+print(bfs_iterative(G, 'Рівне'))
